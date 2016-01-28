6fa49223b10c3a38e5aff1ea92a0219f1cd35709
Tony <t2ly@ucsd.edu>
2016-01-28 18:17:54 +0000

    node.js lab

---
+++ routes/project.js
@@ -0,0 +1,8 @@
+exports.viewProject = function(req, res) {
+  // controller code goes here 
+  var name = req.params.name;
+  console.log("The project name is: " + name);
+  res.render('project', {
+	'projectName': name
+  });
+}
